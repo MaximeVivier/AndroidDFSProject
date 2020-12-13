4b4012f7427be720bd5fd006eac4cd0815ced789
Maxime Vivier <maximevivier92@gmail.com>
2020-12-13 12:09:50 +0100

    alternate views for recycler view

--- app/src/main/java/fr/centralemarseille/maxnews/ArticleAdapter.kt
+++ app/src/main/java/fr/centralemarseille/maxnews/ArticleAdapter.kt
@@ -1,39 +1,55 @@
 package fr.centralemarseille.maxnews
 
-import android.util.Log
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.article_item.view.*
+import com.squareup.picasso.Picasso
+import java.io.IOException
 
 class ArticleAdapter(val articlesList: ArrayList<Article>, private val onArticleClickListener: onArticleClickListener) :
     RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.article_item,
-            parent, false)
+    class ArticleViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView) {
+        val title_view: TextView = itemView.findViewById(R.id.title_view)
+        val auteur_view: TextView = itemView.findViewById(R.id.auteur_view)
+        val date_view: TextView = itemView.findViewById(R.id.date_view)
+        val see_more_button: Button = itemView.findViewById(R.id.see_more_button)
+        val image_article: ImageView = itemView.findViewById(R.id.image_article)
+        val holder_context: Context = context
+    }
 
-        return ArticleViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        if (viewType == 0) {
+            return ArticleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.article_item_left, parent, false), parent.context)
+        } else {
+            return ArticleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.article_item_right, parent, false), parent.context)
+        }
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val Article = articlesList[position]
 
-        holder.textView1.text = Article.title
-        holder.textView2.text = Article.description
-        holder.itemView.see_more.setOnClickListener {
+        holder.title_view.text = Article.title
+        holder.auteur_view.text = Article.author
+        holder.date_view.text = Article.publishedAt
+        holder.see_more_button.setOnClickListener {
             onArticleClickListener.onArticleItemClicked(position)
         }
     }
 
-    override fun getItemCount() = articlesList.size
-
-    class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView1: TextView = itemView.text_view_1
-        val textView2: TextView = itemView.text_view_2
+    override fun getItemViewType(position: Int): Int {
+        return position % 2
     }
+
+    override fun getItemCount() = articlesList.size
 
     fun addArticlesToList (articles: Array<Article>) {
         for (article in articles){
